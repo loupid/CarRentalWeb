99da767883c6b85968486f13566f29e398ec5283
mobenab <mohammed.benabbassi@edu.cegeptr.qc.ca>
2020-10-26 18:01:15 -0400

    Add top navbar design

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Location de voitures</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/styles.css" type="text/css">
+    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+</head>
+<body>
+    <header>
+        <img class="logo" src="Images/Logo.PNG" alt="logo">
+        <div class="search">
+            <input type="text" class="search_box" placeholder="Rechercher...">
+            <button type="submit" class="btn_search"><i class="fa fa-search"></i></button>
+        </div>
+        <nav>
+            <ul class="nav_links">
+                <li><a href="#">À propos de nous</a></li>
+                <li><a href="#">S'inscrire</a></li>
+            </ul>
+        </nav>
+        <a href="#"><button id="btn_login">Se connecter</button></a>
+    </header>
+    <div class="container">
+
+    </div>
+</body>
+</html>
