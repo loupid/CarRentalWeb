4e6557298c685a52768cacb131966891c426caae
mobenab <mohammed.benabbassi@edu.cegeptr.qc.ca>
2020-11-02 15:03:57 -0500

    Side bar menu

--- JS/Main.js
+++ JS/Main.js
@@ -1,11 +1,16 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main_body").style.marginLeft = "250px";
-    document.getElementById("icon_btn_burger").style.color = "#fd5e53";
+    $("#mySidenav").css({"width": "250px"});
+    $("#main_body").css({"marginLeft": "250px"});
+    $("#icon_btn_burger").css({"color": "#fd5e53"});
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main_body").style.marginLeft= "0";
-    document.getElementById("icon_btn_burger").style.color= "#ffffff";
+    $("#mySidenav").css({"width": "0"});
+    $("#main_body").css({"marginLeft": "0"});
+    $("#icon_btn_burger").css({"color": "#ffffff"});
+    $("#icon_btn_burger").hover(function () {
+        $(this).css("color", "#fd5e53")
+    }, function () {
+        $(this).css("color", "#ffffff")
+    });
 }
