2f13e23d5546e21352e35de4c0d76757e1056306
mobenab <mohammed.benabbassi@edu.cegeptr.qc.ca>
2020-11-09 14:27:04 -0500

    quick filter

--- JS/Main.js
+++ JS/Main.js
@@ -2,7 +2,7 @@ function openNav() {
     $("#mySidenav").css({"width": "250px"});
     $("#main_body").css({"marginLeft": "250px"});
     $("#icon_btn_burger").css({"color": "#fd5e53"});
-    $(".quick_filter").css({"margin": "0 15% 10% 15%"});
+    $(".quick_filter").css({"margin": "0 7.5% 10% 7.5%"});
 }
 
 function closeNav() {
